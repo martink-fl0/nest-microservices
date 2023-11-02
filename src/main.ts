f6a151732e838ebbcac11f92d36ba59882103fe3
martinkaldramov <martinkaldramov1@gmail.com>
2023-11-02 14:08:29 +0200

    Created a microservice listening on 3001

--- src/main.ts
+++ src/main.ts
@@ -14,14 +14,20 @@ async function bootstrap() {
    * await app.listen();
    *
    */
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>({
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: { retryAttempts: 5, retryDelay: 3000 },
   });
-
-  await app.startAllMicroservices();
   await app.listen(3001);
+  
+  // const app = await NestFactory.create(AppModule);
+  // app.connectMicroservice<MicroserviceOptions>({
+  //   transport: Transport.TCP,
+  //   options: { retryAttempts: 5, retryDelay: 3000 },
+  // });
+
+  // await app.startAllMicroservices();
+  // await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
