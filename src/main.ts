e2a8e38e97527b9fb0c0a7239f64a981ba016128
martinkaldramov <martinkaldramov1@gmail.com>
2023-11-07 13:26:42 +0200

    Changed port to 3001

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
     transport: Transport.TCP,
     options: {
-      port: 3000
+      port: 3001
     }
   });
   await app.listen();
