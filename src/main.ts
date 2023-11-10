89cd1a8354e9dcc77cbd5e111153a759d6494475
martinkaldramov <martinkaldramov1@gmail.com>
2023-11-10 17:02:14 +0200

    Added express server / endpoint for healthcheck

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as express from 'express';
 
 // Cosmetic change 1
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
+  // Create an HTTP server for health checks
+  const healthCheckServer = express();
+  healthCheckServer.get('/', (_, res) => {
+    console.log("Received a request on '/'");
+    
+    res.status(200).send('OK');
+  });
+  healthCheckServer.listen(3000);
+
+  const microservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: {
       port: 3001
     }
   });
-  console.log("Next line: await app.listen();")
-  await app.listen();
+
+  await microservice.listen(() => {
+    const port = microservice.get('MicroserviceOptions').options.port;
+    console.log(`Microservice is listening on port ${port}`);
+  });
   // console.log("Logging app after await app.listen();", app)
 }
 bootstrap();
